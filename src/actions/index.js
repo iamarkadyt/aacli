1419cb6bc342b17420a67eccb1f0e8dea92541c5
iamarkadyt <pgp-noreply@rkd.dev>
2021-08-30 10:42:58 -0500

    Add web command

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 const { auth } = require('./auth')
+const { web } = require('./web')
 const { config } = require('./config')
 const { crypto } = require('./crypto')
 const { reset } = require('./reset')
 const { unauth } = require('./unauth')
 
-module.exports = { auth, config, crypto, reset, unauth }
+module.exports = { auth, web, config, crypto, reset, unauth }
