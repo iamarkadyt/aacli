ab756240cf038457b2de44e47e9d4ee6467efd95
iamarkadyt <contact.me@arkadyt.dev>
2021-09-05 19:40:58 -0500

    Update run cmd to read from session store

--- src/actions/run.js
+++ src/actions/run.js
@@ -4,13 +4,19 @@ const { ConfUtils, Utils } = require('../helpers')
 async function run(argv) {
     const [config] = await ConfUtils.loadSessionConfig()
     const [command, ...args] = argv
+    const activeSessions = config.filter((session) => new Date(session.expiry) > Date.now())
 
-    if (!Object.keys(config).length) {
-        console.log(`You have no logged in sessions, please authenticate first through "auth" command.`.red)
+    if (!activeSessions.length) {
+        console.log(`You have no logged in sessions, please authenticate first through "auth" command`.red)
         process.exit(1)
     }
 
-    const session = Utils.lodashGet(config, 'default', {})
+    const { session } = await Utils.prompts({
+        type: 'select',
+        name: 'session',
+        message: 'Choose an active session to use',
+        choices: activeSessions.map(([name, sessionObject]) => ({ title: name, value: sessionObject })),
+    })
     const { key, keyId, sessionToken, region } = session
     const env = {
         AWS_ACCESS_KEY_ID: keyId,
